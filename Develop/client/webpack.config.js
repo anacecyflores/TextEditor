aaec6df72faf1446ce5e2be3cc899a4903298894
Cecy Flores <anacecyflores1@gmail.com>
2022-08-28 08:32:14 -0500

    finished adding webpack components

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -23,6 +23,29 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "TextEdit",
+        description:
+          "Text Editor with offline capabilities using IndexedDB, downloadable",
+        background_color: "#200ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
